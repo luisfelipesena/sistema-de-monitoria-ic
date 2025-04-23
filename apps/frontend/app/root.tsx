b7cfd20ccf45e27d0acee2dd778b1d786376017a
Deploy Script <deploy@ic.ufba.br>
2025-04-22 21:49:27 -0300

    fix(frontend): change HTML language attribute to Portuguese

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient();
 
 export default function Root() {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
