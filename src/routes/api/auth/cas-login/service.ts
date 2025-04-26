b4dd12db4a8bfdb63730fc5745c6066ee76bd72c
Deploy Script <deploy@ic.ufba.br>
2025-04-26 01:48:10 -0300

    chore: stop tracking data directory.

--- src/routes/api/auth/cas-login/service.ts
+++ src/routes/api/auth/cas-login/service.ts
@@ -9,7 +9,7 @@ export class CasLoginService {
   getRedirectUrl() {
     const casServerUrlPrefix = env.CAS_SERVER_URL_PREFIX
     const clientUrl = env.CLIENT_URL
-    const serviceUrl = `${clientUrl}/api/auth/cas-callback`
+    const serviceUrl = `${clientUrl}/auth/cas-callback`
     const redirectUrl = `${casServerUrlPrefix}/login?service=${encodeURIComponent(serviceUrl)}`
 
     return redirectUrl
