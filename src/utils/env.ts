95ddb3b4d9666aad0b0b54886c9e9e68daf888ad
antoniel <antoniel2210@gmail.com>
2025-05-10 20:14:43 -0300

    feat(): update npm scripts to use dotenv and improve env variable defaults in env.ts file

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -14,14 +14,14 @@ const envSchema = z.object({
 
 const isWeb = typeof window !== 'undefined';
 export const env = envSchema.parse({
-  DATABASE_URL: isWeb ? '-' : process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL || '',
   CAS_SERVER_URL_PREFIX:
     process.env.CAS_SERVER_URL_PREFIX || 'https://autenticacao.ufba.br/ca',
   SERVER_URL: process.env.SERVER_URL || 'http://localhost:3000/api',
   CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
   NODE_ENV: process.env.NODE_ENV,
-  MINIO_ENDPOINT: isWeb ? '-' : process.env.MINIO_ENDPOINT,
-  MINIO_ACCESS_KEY: isWeb ? '-' : process.env.MINIO_ACCESS_KEY,
-  MINIO_SECRET_KEY: isWeb ? '-' : process.env.MINIO_SECRET_KEY,
-  MINIO_BUCKET_NAME: isWeb ? '-' : process.env.MINIO_BUCKET_NAME,
+  MINIO_ENDPOINT: process.env.MINIO_ENDPOINT || '',
+  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY || '',
+  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY || '',
+  MINIO_BUCKET_NAME: process.env.MINIO_BUCKET_NAME || '',
 });
