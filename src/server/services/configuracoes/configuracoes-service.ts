7d804a3b3809139c196ba9a9fa9cd1e9722769f4
galego-vinicius <vfelix1998@gmail.com>
2026-08-15 09:20:27 -0300

    fix: removendo referencias desnecessarias

--- src/server/services/configuracoes/configuracoes-service.ts
+++ src/server/services/configuracoes/configuracoes-service.ts
@@ -3,8 +3,6 @@ import { ConflictError } from '@/server/lib/errors'
 import { createConfiguracoesRepository } from './configuracoes-repository'
 
 export const EMAIL_IC_CHAVE = 'EMAIL_INSTITUTO_COMPUTACAO'
-export const EMAIL_GERAL_PROFESSORES_CHAVE = 'EMAIL_GERAL_PROFESSORES'
-export const EMAIL_GERAL_ESTUDANTES_CHAVE = 'EMAIL_GERAL_ESTUDANTES'
 
 export interface UpdateDepartamentoEmailInput {
   departamentoId: number
