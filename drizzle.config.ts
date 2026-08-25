76facfff215a5fc5b3d265c21d0863e86bce70ea
Luis Felipe Cordeiro Sena <luisfelipesena@gmail.com>
2026-08-25 13:47:13 +0000

    fix(drizzle): não expor credenciais nos logs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'drizzle-kit'
 
 import { env } from './src/utils/env'
 
-console.log(env.DATABASE_URL)
 export default defineConfig({
   schema: './src/server/db/schema.ts',
   out: './drizzle',
