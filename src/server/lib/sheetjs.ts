6b2ea844bcbb7004c825e2a0e388478a4fd129eb
gabsizinio <gabriel.cruz@jusbrasil.com.br>
2026-08-26 09:16:06 -0300

    chore: export XLSX typed export in sheetjs

--- src/server/lib/sheetjs.ts
+++ src/server/lib/sheetjs.ts
@@ -2,7 +2,7 @@ import * as XLSXModule from 'xlsx'
 // @ts-ignore
 import * as codepages from 'xlsx/dist/cpexcel.full.mjs'
 
-const XLSX = (XLSXModule as any).default || XLSXModule
+const XLSX = ((XLSXModule as any).default || XLSXModule) as typeof XLSXModule
 if (typeof (XLSX as any).set_cptable === 'function') {
   ;(XLSX as any).set_cptable(codepages)
 }
